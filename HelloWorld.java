afe30ee8ef48f4b2f316348649ff93eb82ce61d1
sriniladdu81 <sriniladdu81@gmail.com>
2025-06-05 17:14:28 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Sridhar World!"); 
+        System.out.println("Hello, Sridhar World2!"); 
     }
 }
