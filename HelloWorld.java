1b1d8cc71dc9e07c64f1213051f96da821cc9708
sriniladdu81 <sriniladdu81@gmail.com>
2025-06-09 18:38:04 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Sridhar World2!"); 
+        System.out.println("Hello, Sridhar!, This is Jenkins Pipeline practicals"); 
     }
 }
